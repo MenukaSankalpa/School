daaca6a5b1f4a04f83f734cfdcc67fa4ceebfe1e
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-08-02 23:04:30 +0530

    lohin page add admins route

--- login.php
+++ login.php
@@ -3,35 +3,51 @@
 include 'db.php';
 
 $userInput = $_POST['email'];
-$password = md5($_POST['password']);
+$password = md5($_POST['password']); 
 
-$sql = "SELECT * FROM users WHERE (email=? OR username=? ) AND password=?";
+session_start();
+
+// Check in users table
+$sql = "SELECT * FROM users WHERE (email = ? OR username = ?) AND password = ?";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("sss", $userInput, $userInput, $password);
 $stmt->execute();
 $result = $stmt->get_result();
 
-if($result->num_rows === 1) {
+if ($result->num_rows === 1) {
     $user = $result->fetch_assoc();
-    $role = $user['role'];
-
-    session_start();
     $_SESSION['email'] = $user['email'];
     $_SESSION['user_id'] = $user['id'];
-    $_SESSION['role'] = $role;
+    $_SESSION['role'] = $user['role'];
 
-    //Redirect pages based on roles 
-    if ($role == '1'){
+    if ($user['role'] == '1') {
         header("Location: parent/parent_dash.php");
-    } elseif ($role == '2') {
+    } elseif ($user['role'] == '2') {
         header("Location: admin_dashboard.php");
-    } elseif ($role == '3') {
-        header("Location: super_admin_dashboard.php");
+    } elseif ($user['role'] == '3') {
+        header("Location: admin/layout.php");
     }
-} else {
-    echo ("<script>alert('Invalid Login Credentials!'); window.location.href='index.html'</script>");
+    exit;
 }
 
-$conn->close();
+// check admins table
+$sql_admin = "SELECT * FROM admins WHERE (email = ? OR username = ?) AND password = ?";
+$stmt_admin = $conn->prepare($sql_admin);
+$stmt_admin->bind_param("sss", $userInput, $userInput, $password);
+$stmt_admin->execute();
+$result_admin = $stmt_admin->get_result();
 
+if ($result_admin->num_rows === 1) {
+    $admin = $result_admin->fetch_assoc();
+    $_SESSION['email'] = $admin['email'];
+    $_SESSION['user_id'] = $admin['id'];
+    $_SESSION['role'] = '3'; 
+
+    header("Location: school_admin/admin_dashboard.php");
+    exit;
+}
+
+
+echo ("<script>alert('Invalid Login Credentials!'); window.location.href='index.html'</script>");
+$conn->close();
 ?>
